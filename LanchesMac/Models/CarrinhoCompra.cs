6b29655b6b68ace7e58f7918dce97e5e0d0df63e
Jefferson Ferreira <jefferson14489@gmail.com>
2023-11-14 17:48:55 -0300

    Concluindo o Carrinho de Compras

---
+++ LanchesMac/Models/CarrinhoCompra.cs
@@ -0,0 +1,107 @@
+﻿using LanchesMac.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Models
+{
+    public class CarrinhoCompra
+    {
+        private readonly AppDbContext _context;
+        public CarrinhoCompra(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+        public string CarrinhoCompraId { get; set; }
+
+        public List<CarrinhoCompraItem> CarrinhoCompraItems { get; set; }
+
+        public static CarrinhoCompra GetCarrinho(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            var context = services.GetService<AppDbContext>();
+
+            string carrinhoId = session.GetString("CarrinhoId") ?? Guid.NewGuid().ToString();
+
+            session.SetString("CarrinhoId", carrinhoId);
+
+            return new CarrinhoCompra(context)
+            {
+                CarrinhoCompraId = carrinhoId
+            };
+        }
+
+        public void AdicionarAoCarrinho(Lanche lanche)
+        {
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(
+                carrinho => carrinho.Lanche.LancheId == lanche.LancheId && carrinho.CarrinhoCompraId == CarrinhoCompraId
+                );
+
+            if(carrinhoCompraItem == null) {
+
+                carrinhoCompraItem = new CarrinhoCompraItem()
+                {
+                    CarrinhoCompraId = CarrinhoCompraId,
+                    Lanche = lanche,
+                    Quantidade = 1,
+                };
+
+                _context.CarrinhoCompraItens.Add(carrinhoCompraItem);
+
+            }
+            else
+            {
+                carrinhoCompraItem.Quantidade++;
+            }
+
+            _context.SaveChanges();
+        }
+
+        public int RemoverDoCarrinho(Lanche lanche)
+        {
+            var carrinhoCompraItem = _context.CarrinhoCompraItens.SingleOrDefault(
+                carrinho => carrinho.Lanche.LancheId == lanche.LancheId && carrinho.CarrinhoCompraId == CarrinhoCompraId
+                );
+
+            var quantidadeLocal = 0;
+            
+            if(carrinhoCompraItem != null)
+            {
+                if(carrinhoCompraItem.Quantidade > 1)
+                {
+                    carrinhoCompraItem.Quantidade--;
+                    quantidadeLocal = carrinhoCompraItem.Quantidade;
+                }
+                else
+                {
+                    _context.CarrinhoCompraItens.Remove(carrinhoCompraItem);
+                }
+            }
+
+            _context.SaveChanges();
+            return quantidadeLocal;
+        }
+
+        public List<CarrinhoCompraItem> GetCarrinhoCompraItens()
+        {
+            return CarrinhoCompraItems ?? (CarrinhoCompraItems = _context.CarrinhoCompraItens.Where(carrinho => carrinho.CarrinhoCompraId == CarrinhoCompraId).Include(lanche => lanche.Lanche).ToList());
+        }
+
+        public void LimparCarrinho()
+        {
+            var carrinhoItens = _context.CarrinhoCompraItens.Where(carrinho => carrinho.CarrinhoCompraId == CarrinhoCompraId);
+            _context.CarrinhoCompraItens.RemoveRange(carrinhoItens);
+
+            _context.SaveChanges();
+        }
+
+        public decimal GetCarrinhoCompraTotal()
+        {
+            var total  = _context.CarrinhoCompraItens
+                        .Where(carrinho => carrinho.CarrinhoCompraId == CarrinhoCompraId)
+                        .Select(carrinho => carrinho.Lanche.Preco * carrinho.Quantidade)
+                        .Sum();
+
+            return total;
+        }
+    }
+}
---
+++ LanchesMac/Models/CarrinhoCompraItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
+{
+    [Table("CarrinhoCompraItens")]
+    public class CarrinhoCompraItem
+    {
+        public int CarrinhoCompraitemId { get; set; }
+        public Lanche Lanche { get; set; }
+        public int Quantidade { get; set; }
+        public string CarrinhoCompraId { get; set; }
+    }
+}
